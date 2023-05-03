955c8ccb4eb02c5de1ece1697f5151a51eb72a66
LuiArm <Luisarmendariz24@icloud.com>
2023-05-02 18:16:20 -0600

    Initial Commit

---
+++ MathWizz/MathWizzApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathWizzApp.swift
+//  MathWizz
+//
+//  Created by luis armendariz on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct MathWizzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
